899d2fd9626c3d7be24adde43cd898b4fb856377
Jorge Monteiro <Jorge Monteiro@DESKTOP-RT73T4G>
2020-02-26 14:51:22 +0000

    Criação do Modelo, Controlador e View do Produto!

---
+++ FT_Management/Controllers/ProdutosController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FT_Management.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        // GET: Produtos
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Produtos/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Produtos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Produtos/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Produtos/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Produtos/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Produtos/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Produtos/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ FT_Management/Models/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FT_Management.Models
+{
+    public class Produto
+    {
+        public int Id_Produto { get; set; }
+        public string Ref_Produto { get; set; }
+        public string Designacao_Produto { get; set; }
+        public double Stock_Fisico { get; set; }
+        public double Stock_PHC { get; set; }
+        public string Pos_Stock { get; set; }
+        public string Obs_Produto { get; set; }
+    }
+}
