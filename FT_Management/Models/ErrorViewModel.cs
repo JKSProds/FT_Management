e6b4f844092229c044b36d052c2c2b0199e0e688
Jorge Monteiro <Jorge Monteiro@DESKTOP-RT73T4G>
2020-02-26 14:18:51 +0000

    Add project files.

---
+++ FT_Management/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FT_Management.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
