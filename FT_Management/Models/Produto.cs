4317c6b89c1c0ac26958ee1a1389b024eb9acaf0
Jorge Monteiro <Jorge Monteiro@DESKTOP-RT73T4G>
2020-02-28 10:55:13 +0000

    Alterações visuais á listagem de referencias.

--- FT_Management/Models/Produto.cs
+++ FT_Management/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,17 @@ namespace FT_Management.Models
 {
     public class Produto
     {
+        [Display(Name = "Referência")]
         public string Ref_Produto { get; set; }
+        [Display(Name = "Designação")]
         public string Designacao_Produto { get; set; }
+        [Display(Name = "Stock Fisico")]
         public double Stock_Fisico { get; set; }
+        [Display(Name = "PHC")]
         public double Stock_PHC { get; set; }
+        [Display(Name = "Localização")]
         public string Pos_Stock { get; set; }
+        [Display(Name = "Observações")]
         public string Obs_Produto { get; set; }
     }
 }
