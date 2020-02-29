2f36022b7bd0c323a2598d1997bfc3da20cd0e36
Jorge Monteiro <Jorge Monteiro@DESKTOP-RT73T4G>
2020-02-29 12:46:45 +0000

    Criação de Deploy e Docker file

---
+++ DEPLOY.sh
@@ -0,0 +1,22 @@
+cd ~/FT_Management
+
+git clone https://github.com/JKSProds/FT_Management.git
+
+cd FT_Management/FT_Management
+
+docker build -t FT_Management .
+
+docker stop FT_Management
+docker rm FT_Management
+
+cd ..
+cd ..
+
+chmod -R 777 FT_Management
+
+rm -r FT_Management
+
+docker run -d -p 8080:80 --restart always --name FT_Management FT_Management
+
+
+
