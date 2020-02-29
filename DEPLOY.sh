8658292465c0ff33c2d328497f307864b62d2ed7
Jorge Monteiro <Jorge Monteiro@DESKTOP-RT73T4G>
2020-02-29 12:49:31 +0000

    Fix deploy

--- DEPLOY.sh
+++ DEPLOY.sh
@@ -4,7 +4,7 @@ git clone https://github.com/JKSProds/FT_Management.git
 
 cd FT_Management/FT_Management
 
-docker build -t FT_Management .
+docker build -t ftmanagement .
 
 docker stop FT_Management
 docker rm FT_Management
@@ -16,7 +16,7 @@ chmod -R 777 FT_Management
 
 rm -r FT_Management
 
-docker run -d -p 8080:80 --restart always --name FT_Management FT_Management
+docker run -d -p 8080:80 --restart always --name FT_Management ftmanagement
 
 
 
