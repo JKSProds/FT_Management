e059329b1f0eeaf44ee4303fe3e7b993bde7d374
Jorge Monteiro <Jorge Monteiro@DESKTOP-RT73T4G>
2020-07-05 23:30:47 +0100

    ...

--- FT_Management/Startup.cs
+++ FT_Management/Startup.cs
@@ -42,10 +42,14 @@ namespace FT_Management
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseExceptionHandler("/Home/Error");
+                //// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                //app.UseHsts();
+                app.UseDeveloperExceptionPage();
+
             }
+
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
