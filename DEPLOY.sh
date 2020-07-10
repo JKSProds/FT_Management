3c92f7df8a815e7d13854eedc7010b794053ecf2
Jorge Monteiro <Jorge Monteiro@DESKTOP-RT73T4G>
2020-07-10 23:00:50 +0100

    Modificado script deploy

--- DEPLOY.sh
+++ DEPLOY.sh
@@ -2,6 +2,8 @@ cd ~/FT_Management
 
 git clone https://github.com/JKSProds/FT_Management.git
 
+cp appsettings.json FT_Management/FT_Management/appsettings.json
+
 cd FT_Management/FT_Management
 
 docker build -t ftmanagement .
@@ -16,7 +18,7 @@ chmod -R 777 FT_Management
 
 rm -r FT_Management
 
-docker run -d -p 8082:80 --restart always --name FT_Management ftmanagement
+docker run -d -p 8082:80 --restart always --name FT_Management ftmanagement 
 
-
+docker image prune -a
 
