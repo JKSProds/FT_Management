d7d84cf38712edb32f8248e1d5db03599bc819e7
Jorge Monteiro <Jorge Monteiro@DESKTOP-RT73T4G>
2020-07-22 17:36:26 +0100

    bug fixes

--- FT_Management/Models/FT_ManagementContext.cs
+++ FT_Management/Models/FT_ManagementContext.cs
@@ -984,7 +984,7 @@ namespace FT_Management.Models
                     pdfFormFields.SetFieldProperty("DesignaçãoRow" + p, "textsize", 6f, null);
                     if (pecas.Designacao_Produto.Length > 50)
                     {
-                        pdfFormFields.SetField("DesignaçãoRow" + p, pecas.Designacao_Produto.Substring(0,54));
+                        pdfFormFields.SetField("DesignaçãoRow" + p, pecas.Designacao_Produto.Substring(0,50));
                     }
                     else
                     {
