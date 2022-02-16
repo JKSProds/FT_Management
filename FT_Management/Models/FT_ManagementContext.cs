97f55bcff41c70ed48c8c3941095a57822143aa2
Jorge Monteiro (FT-PC) <jmonteiro@FT>
2022-02-16 21:08:30 +0000

    Adicionado indicacao de inicio de dia pela app

--- FT_Management/Models/FT_ManagementContext.cs
+++ FT_Management/Models/FT_ManagementContext.cs
@@ -2145,13 +2145,13 @@ namespace FT_Management.Models
             if (LstAcessos.Count > 0)
             {
                 string sql1 = "INSERT INTO dat_acessos (IdUtilizador,DataHoraAcesso,Tipo, Temperatura, App) VALUES ";
-                string sql2 = "INSERT INTO dat_acessos_utilizador (IdUtilizador, DataUltimoAcesso, TipoUltimoAcesso) VALUES";
+                string sql2 = "INSERT INTO dat_acessos_utilizador (IdUtilizador, DataUltimoAcesso, TipoUltimoAcesso, App) VALUES";
 
                 foreach (Acesso acesso in LstAcessos.OrderBy(a => a.Data))
                 {
                     sql1 += "((SELECT IdUtilizador FROM sys_utilizadores WHERE IdPHC = " + acesso.IdUtilizador + "), '" + acesso.Data.ToString("yyyy-MM-dd HH:mm:ss") + "', " + acesso.Tipo + ", '" + acesso.Temperatura + "', 1),\r\n";
 
-                    if (acesso.Data > ObterUltimoAcesso(acesso.IdUtilizador)) sql2 += "((SELECT IdUtilizador FROM sys_utilizadores WHERE IdPHC = " + acesso.IdUtilizador + "), '" + acesso.Data.ToString("yyyy-MM-dd HH:mm:ss") + "', " + acesso.Tipo + "),\r\n";
+                    if (acesso.Data > ObterUltimoAcesso(acesso.IdUtilizador)) sql2 += "((SELECT IdUtilizador FROM sys_utilizadores WHERE IdPHC = " + acesso.IdUtilizador + "), '" + acesso.Data.ToString("yyyy-MM-dd HH:mm:ss") + "', " + acesso.Tipo + ", 1),\r\n";
                 }
 
                 sql1 = sql1.Remove(sql1.Count() - 3);
