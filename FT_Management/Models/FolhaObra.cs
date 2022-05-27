3873e7d7fd0291b72d8bdfbb0152f5ab22619176
Jorge Monteiro (FT-PC) <jmonteiro@FT>
2022-05-27 18:31:18 +0100

    Alterações á interface das marcações

--- FT_Management/Models/FolhaObra.cs
+++ FT_Management/Models/FolhaObra.cs
@@ -351,22 +351,32 @@ namespace FT_Management.Models
 
     public class Marcacao
     {
+        [Display(Name = "Num. da Marcação")]
         public int IdMarcacao { get; set; }
+        [Display(Name = "Agendado para:")]
         public DateTime DataMarcacao { get; set; }
         public Cliente Cliente { get; set; }
+        [Display(Name = "Criado em:")]
         public DateTime DataCriacao { get; set; }
         public int IdTecnico { get; set; }
         public Utilizador Tecnico { get; set; }
         public string ResumoMarcacao { get; set; }
         public int EstadoMarcacao { get; set; }
+        [Display(Name = "Estado Atual")]
         public string EstadoMarcacaoDesc { get; set; }
+        [Display(Name = "Prioridade")]
         public string PrioridadeMarcacao { get; set; }
         public string MarcacaoStamp { get; set; }
         public List<FolhaObra> LstFolhasObra { get; set; }
         public List<Comentario> LstComentarios { get; set; }
         public int Oficina { get; set; }
         public int Instalacao { get; set; }
+        [Display(Name = "Equipamento")]
         public string TipoEquipamento { get; set; }
+        [Display(Name = "Periodo")]
+        public string Periodo { get; set; }
+        [Display(Name = "Incidente")]
+        public string Referencia { get; set; }
     }
 
     public class Visita
