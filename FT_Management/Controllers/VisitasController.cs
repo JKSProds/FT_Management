81974f26e84f04f01574aecf421ab2b111274633
Jorge Monteiro (FT-PC) <jmonteiro@FT>
2022-06-08 09:56:57 +0100

    Melhoramentos ao controlador das visitas

--- FT_Management/Controllers/VisitasController.cs
+++ FT_Management/Controllers/VisitasController.cs
@@ -24,12 +24,14 @@ namespace FT_Management.Controllers
             return new JsonResult(context.ConverterVisitasEventos(context.ObterListaVisitas(start, end)).ToList());
 
         }
+
         public JsonResult ObterVisitasComercial(DateTime start, DateTime end)
         {
             FT_ManagementContext context = HttpContext.RequestServices.GetService(typeof(FT_ManagementContext)) as FT_ManagementContext;
             return new JsonResult(context.ConverterVisitasEventos(context.ObterListaVisitas(int.Parse(this.User.Claims.First().Value), start, end)).ToList());
 
         }
+
         [Authorize(Roles = "Admin, Escritorio")]
         public ActionResult Calendario()
         {
@@ -54,41 +56,37 @@ namespace FT_Management.Controllers
             context.CriarVisitas(LstVisita);
             return Json("ok");
         }
-        // GET: VisitasController
+
         public ActionResult Index()
         {
+            if (!User.IsInRole("Admin") && !User.IsInRole("Escritorio")) return RedirectToAction("ListaVisitas");
+
             FT_ManagementContext context = HttpContext.RequestServices.GetService(typeof(FT_ManagementContext)) as FT_ManagementContext;
 
-            if (!User.IsInRole("Admin") && !User.IsInRole("Escritorio"))
-            {
-                return RedirectToAction("ListaVisitas");
-            }
-            else
-            {
-                return View(context.ObterListaComerciais());
-            }
+   \        return View(context.ObterListaComerciais());
         }
 
         [HttpPost]
         public JsonResult ObterClientes(string prefix)
         {
-            if (prefix is null) prefix = "";
             FT_ManagementContext context = HttpContext.RequestServices.GetService(typeof(FT_ManagementContext)) as FT_ManagementContext;
+
+            if (prefix is null) prefix = "";
 
             return Json(context.ObterListaClientes(prefix, false));
         }
 
         public ActionResult ListaVisitas(int IdComercial, string DataVisitas)
         {
-            FT_ManagementContext context = HttpContext.RequestServices.GetService(typeof(FT_ManagementContext)) as FT_ManagementContext;
-
             if (!User.IsInRole("Admin") && !User.IsInRole("Escritorio")) IdComercial = int.Parse(this.User.Claims.First().Value);
-
             if (DataVisitas == null || DataVisitas == string.Empty) DataVisitas = DateTime.Now.ToString("dd-MM-yyyy");
-            ViewData["DataVisitas"] = DataVisitas;
 
+            FT_ManagementContext context = HttpContext.RequestServices.GetService(typeof(FT_ManagementContext)) as FT_ManagementContext;
             List<Visita> ListaVisitas = context.ObterListaVisitas(IdComercial, DateTime.Parse(DataVisitas), DateTime.Parse(DataVisitas));
+
+            ViewData["DataVisitas"] = DataVisitas;
             ViewData["IdComercial"] = IdComercial;
+
             return View(ListaVisitas);
         }
 
@@ -98,35 +96,37 @@ namespace FT_Management.Controllers
             FT_ManagementContext context = HttpContext.RequestServices.GetService(typeof(FT_ManagementContext)) as FT_ManagementContext;
 
             ViewData["Comerciais"] = context.ObterListaUtilizadores().Where(u => u.TipoUtilizador == 2).ToList();
+
             return View();
         }
+
         [HttpPost]
         public ActionResult Adicionar(int IdCliente, int IdLoja, DateTime txtData, int txtComercial, string Obs)
         {
-
             if (IdCliente == 0 && IdLoja == 0 && txtComercial == 0) return View();
 
             FT_ManagementContext context = HttpContext.RequestServices.GetService(typeof(FT_ManagementContext)) as FT_ManagementContext;
-                Visita visita = new Visita()
-                {
-                    IdVisita = 0,
-                    DataVisita = txtData,
-                    Cliente = new Cliente()
-                    {
-                        IdCliente = IdCliente,
-                        IdLoja = IdLoja
-                    },
-                    IdComercial = txtComercial,
-                    ResumoVisita = Obs,
-                    ObsVisita = "",
-                    EstadoVisita = "Agendado",
-                    Contacto = new Contacto() { IdContacto = 0}
-                };
-                List<Visita> lstVisitas = new List<Visita>();
-                lstVisitas.Add(visita);
 
-                context.CriarVisitas(lstVisitas);
-                context.AdicionarLog(context.ObterUtilizador(int.Parse(this.User.Claims.First().Value)).NomeUtilizador, "Foi adicionada uma visita nova ao cliente: " + IdCliente, 5);
+            Visita visita = new Visita()
+            {
+                IdVisita = 0,
+                DataVisita = txtData,
+                Cliente = new Cliente()
+                {
+                    IdCliente = IdCliente,
+                    IdLoja = IdLoja
+                },
+                IdComercial = txtComercial,
+                ResumoVisita = Obs,
+                ObsVisita = "",
+                EstadoVisita = "Agendado",
+                Contacto = new Contacto() { IdContacto = 0}
+            };
+
+            List<Visita> lstVisitas = new List<Visita>();
+            lstVisitas.Add(visita);
+
+            context.CriarVisitas(lstVisitas);
 
             return RedirectToAction("Index", "Visitas");
 
@@ -135,45 +135,48 @@ namespace FT_Management.Controllers
         [Authorize(Roles = "Admin, Escritorio")]
         public ActionResult Editar(int idVisita)
         {
-            ViewData["ReturnUrl"] = Request.Query["ReturnUrl"];
             FT_ManagementContext context = HttpContext.RequestServices.GetService(typeof(FT_ManagementContext)) as FT_ManagementContext;
-
+           
+            ViewData["ReturnUrl"] = Request.Query["ReturnUrl"];
             ViewData["Comerciais"] = context.ObterListaUtilizadores().Where(u => u.TipoUtilizador == 2).ToList();
+
             return View(context.ObterVisita(idVisita));
         }
+
         [Authorize(Roles = "Admin, Escritorio")]
         [HttpPost]
         public ActionResult Editar(Visita visita, string ReturnUrl)
         {
             FT_ManagementContext context = HttpContext.RequestServices.GetService(typeof(FT_ManagementContext)) as FT_ManagementContext;
             List<Visita> lstVisitas = new List<Visita>();
+
             lstVisitas.Add(visita);
             context.CriarVisitas(lstVisitas);
+
             return Redirect(ReturnUrl);
         }
 
         public ActionResult Visita(int idVisita, int IdComercial)
         {
-            FT_ManagementContext context = HttpContext.RequestServices.GetService(typeof(FT_ManagementContext)) as FT_ManagementContext;
-
             if (!User.IsInRole("Admin") && !User.IsInRole("Escritorio")) IdComercial = context.ObterUtilizador(int.Parse(this.User.Claims.First().Value)).Id;
-            ViewData["ReturnUrl"] = Request.Query["ReturnUrl"];
 
+            FT_ManagementContext context = HttpContext.RequestServices.GetService(typeof(FT_ManagementContext)) as FT_ManagementContext;
             Visita visita = context.ObterVisita(idVisita);
 
             if (visita.IdComercial == IdComercial || User.IsInRole("Admin")) return View(visita);
 
+            ViewData["ReturnUrl"] = Request.Query["ReturnUrl"];
+
             return Redirect(Request.Query["ReturnUrl"]);
         }
+
         [HttpPost]
         public ActionResult Visita(Visita visita)
         {
             FT_ManagementContext context = HttpContext.RequestServices.GetService(typeof(FT_ManagementContext)) as FT_ManagementContext;
-
             List<Visita> LstVisitas = new List<Visita> { visita };
 
             context.CriarVisitas(LstVisitas);
-
 
             return Redirect(Request.Query["ReturnUrl"]);
         }
@@ -184,6 +187,7 @@ namespace FT_Management.Controllers
         {
 
             FT_ManagementContext context = HttpContext.RequestServices.GetService(typeof(FT_ManagementContext)) as FT_ManagementContext;
+
             context.ApagarVisita(idVisita);
 
             return Redirect(ReturnUrl);
@@ -205,9 +209,10 @@ namespace FT_Management.Controllers
         {
             FT_ManagementContext context = HttpContext.RequestServices.GetService(typeof(FT_ManagementContext)) as FT_ManagementContext;
             Visita visita = context.ObterVisita(IdVisita);
+            List<Proposta> LstPropostas = new List<Proposta>();
+
             if (files.Count > 0)
             {
-                List<Proposta> LstPropostas = new List<Proposta>();
 
                 LstPropostas.Add(new Proposta()
                 {
@@ -223,12 +228,12 @@ namespace FT_Management.Controllers
                 context.CriarPropostas(LstPropostas);
                 EnviarNextCloud(files, ConfigurationManager.AppSetting["NextCloud:URL"], "Propostas", visita.Cliente.NomeCliente);
             }
+
             return Redirect(Request.Query["ReturnUrl"]);
         }
 
         public async void EnviarNextCloud(List<IFormFile> files, string Url, string Path, string Folder)
         {
-
             foreach (var formFile in files)
             {
                 if (formFile.Length > 0)
@@ -237,14 +242,12 @@ namespace FT_Management.Controllers
                     await formFile.CopyToAsync(ms);
                     ms.Seek(0, SeekOrigin.Begin);
 
-
                     var clientParams = new WebDavClientParams
                     {
                         BaseAddress = new Uri(Url),
                         Credentials = new NetworkCredential(ConfigurationManager.AppSetting["NextCloud:User"], ConfigurationManager.AppSetting["NextCloud:Password"])
                     };
                     var client = new WebDavClient(clientParams);
-
 
                     await client.Mkcol(Folder + "/");
                     await client.Mkcol(Folder + "/" + Path + "/");
@@ -265,12 +268,12 @@ namespace FT_Management.Controllers
                 BaseAddress = new Uri(Url),
                 Credentials = new NetworkCredential(ConfigurationManager.AppSetting["NextCloud:User"], ConfigurationManager.AppSetting["NextCloud:Password"])
             };
+
             var client = new WebDavClient(clientParams);
 
             using (var response = await client.GetRawFile(Url))
             using (var reader = new StreamReader(response.Stream))
             {
-
                 var bytes = default(byte[]);
                 using (var memstream = new MemoryStream())
                 {
@@ -290,9 +293,6 @@ namespace FT_Management.Controllers
 
                 return File(bytes, contentType);
             }
-
-          
         }
-
     }
 }
