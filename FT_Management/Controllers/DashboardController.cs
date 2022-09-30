38314142848e5c2846bd02b17f8985f9a6fe3d36
Jorge Monteiro (FT-PC) <jmonteiro@FT>
2022-09-30 15:21:16 +0100

    Alterações á APIKEY

--- FT_Management/Controllers/DashboardController.cs
+++ FT_Management/Controllers/DashboardController.cs
@@ -63,13 +63,13 @@ namespace FT_Management.Controllers
     {
 
         [AllowAnonymous]
-        public IActionResult Encomendas(string ApiKey)
+        public IActionResult Encomendas(string Api)
         {
             PHCContext phccontext = HttpContext.RequestServices.GetService(typeof(PHCContext)) as PHCContext;
             FT_ManagementContext context = HttpContext.RequestServices.GetService(typeof(FT_ManagementContext)) as FT_ManagementContext;
 
-            int IdUtilizador = context.ObterIdUtilizadorApiKey(ApiKey);
-            if (String.IsNullOrEmpty(ApiKey) && User.Identity.IsAuthenticated) IdUtilizador = int.Parse(this.User.Claims.First().Value);
+            int IdUtilizador = context.ObterIdUtilizadorApiKey(Api);
+            if (String.IsNullOrEmpty(Api) && User.Identity.IsAuthenticated) IdUtilizador = int.Parse(this.User.Claims.First().Value);
             if (IdUtilizador == 0) return Forbid();
 
             return View(phccontext.ObterEncomendas());
