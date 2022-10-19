2308d7e0dd5d325a4ebe84c9ba6a52a3e4fa440a
Jorge Monteiro (FT-PC) <jmonteiro@FT>
2022-10-19 17:24:47 +0100

    Adicionados estados encomenda de fornecedor. Aidiconado chats

--- FT_Management/Controllers/DashboardController.cs
+++ FT_Management/Controllers/DashboardController.cs
@@ -35,5 +35,17 @@ namespace FT_Management.Controllers
 
             return View(phccontext.ObterMarcacoesPendentes());
         }
+        [AllowAnonymous]
+        public IActionResult Marcacoes(string Api)
+        {
+            PHCContext phccontext = HttpContext.RequestServices.GetService(typeof(PHCContext)) as PHCContext;
+            FT_ManagementContext context = HttpContext.RequestServices.GetService(typeof(FT_ManagementContext)) as FT_ManagementContext;
+
+            int IdUtilizador = context.ObterIdUtilizadorApiKey(Api);
+            if (String.IsNullOrEmpty(Api) && User.Identity.IsAuthenticated) IdUtilizador = int.Parse(this.User.Claims.First().Value);
+            if (IdUtilizador == 0) return Forbid();
+
+            return View();
+        }
     }
 }
