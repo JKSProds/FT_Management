2b5d08a15a73fc1ee4e26f0b154010b53170721d
Jorge Monteiro (FT-PC) <jmonteiro@FT>
2022-12-02 09:23:52 +0000

    Pequenas alteraçaos ao estado do agendamento

--- FT_Management/Models/FT_ManagementContext.cs
+++ FT_Management/Models/FT_ManagementContext.cs
@@ -1872,7 +1872,7 @@ namespace FT_Management.Models
                             id = item.IdMarcacao.ToString() + "_" + dataMarcacao.ToString("yyyyMMdd"),
                             IdMarcacao = item.IdMarcacao,
                             calendarId = "1",
-                            title = (item.EstadoMarcacao == 4 || item.EstadoMarcacao == 9 || item.EstadoMarcacao == 10 ? "✔ " : item.EstadoMarcacao != 1 && item.EstadoMarcacao != 26  ? "⌛ " : item.DataMarcacao < DateTime.Now ? "❌ " : "") + item.Cliente.NomeCliente,
+                            title = (item.EstadoMarcacao == 4 || item.EstadoMarcacao == 9 || item.EstadoMarcacao == 10 ? "✔ " : item.EstadoMarcacao != 1 && item.EstadoMarcacao != 26  ? "⌛ " : item.EstadoMarcacaoDesc == "Criado" && item.Utilizador.NomeCompleto == "MailTrack" ? "🤖 " : item.DataMarcacao < DateTime.Now && item.EstadoMarcacaoDesc != "Criado" ? "❌ " : "") + item.Cliente.NomeCliente,
                             start = dataMarcacao,
                             end = dataMarcacao.AddMinutes(25),
                             IdTecnico = item.Tecnico.Id,
