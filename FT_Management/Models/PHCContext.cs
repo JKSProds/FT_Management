70f7c0892ae9dd39d1e2e62e51915c5b5f87846c
Jorge Monteiro (MacBook) <jmonteiro@food-tech.pt>
2023-01-20 14:18:58 +0000

    Adicionados marcacaoes rececionadas

--- FT_Management/Models/PHCContext.cs
+++ FT_Management/Models/PHCContext.cs
@@ -1534,7 +1534,7 @@ namespace FT_Management.Models
             //List<Marcacao> LstMarcacoes = ObterMarcacoes("SELECT num, data, (SELECT TOP 1 SUBSTRING((SELECT ';'+u_mtecnicos.tecnno  AS [text()] FROM u_mtecnicos WHERE u_mtecnicos.marcacaostamp=u_marcacao.u_marcacaostamp and marcado=1 ORDER BY tecnno FOR XML PATH (''), TYPE).value('text()[1]','nvarchar(max)'), 2, 1000)FROM u_mtecnicos) as LstTecnicos, (select string_agg(CONVERT(VARCHAR(10),data,120), '|') from u_mdatas where u_mdatas.u_marcacaostamp = u_marcacao.u_marcacaostamp) as u_mdatas, no, estab, u_mtecnicos.tecnno, tipoe, tipos, resumo, estado, periodo, prioridade, u_marcacaostamp, oficina, piquete, nincidente, datapedido, tipopedido, qpediu, respemail, resptlm, hora, u_marcacao.ousrdata, u_marcacao.ousrhora, u_marcacao.ousrinis  FROM u_marcacao full outer join u_mtecnicos on u_mtecnicos.marcacaostamp = u_marcacao.u_marcacaostamp and u_mtecnicos.marcado=1 WHERE estado not in ('AT Validada', 'Aguarda Ped. Compra') order by estado;", false, false, false, false, false, false);
             //return LstMarcacoes;
 
-            return ObterMarcacoes("select * from v_marcacoes WHERE estado='Criado';", false, true, true, false, false, false).OrderBy(m => m.IdMarcacao).ToList();
+            return ObterMarcacoes("select * from v_marcacoes WHERE estado='Criado' or estado='Rececionado';", false, true, true, false, false, false).OrderBy(m => m.IdMarcacao).ToList();
 
         }
         public List<Marcacao> ObterMarcacoesPendentes(int IdTecnico)
