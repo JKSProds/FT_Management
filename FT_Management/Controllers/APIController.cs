40094ec7dee88030002e30889b36da639ef2876c
Jorge Monteiro <jmonteiro@food-tech.pt>
2024-03-26 17:13:26 +0000

    Deploy de dashboard de assistencias tecnicas

--- FT_Management/Controllers/APIController.cs
+++ FT_Management/Controllers/APIController.cs
@@ -28,5 +28,35 @@ namespace FT_Management.Controllers
 
             return MailContext.EnviarEmailManual(n.UtilizadorDestino.EmailUtilizador,n.Assunto,n.Mensagem,n.Cc) ? (phccontext.FecharEmail(id)[0] != "0" ? StatusCode(200) : StatusCode(500)) : StatusCode(500);
         }
+
+
+        //Obter dados para graficos
+        [HttpGet("API/Graficos/Assistencias")]
+        public IActionResult GraficoAssistencias(DateTime dInicio, DateTime dFim, int c, string cl)
+        {
+            // Retrieve data from your data source
+            FT_ManagementContext context = HttpContext.RequestServices.GetService(typeof(FT_ManagementContext)) as FT_ManagementContext;
+            PHCContext phccontext = HttpContext.RequestServices.GetService(typeof(PHCContext)) as PHCContext;
+            
+            8
+            var data = new {
+                labels = d.Keys,
+                values = d.Values
+            };
+            return Ok(data);
+        }
+
+        //Obter dados para graficos
+        [HttpGet("API/Dados/Assistencias")]
+        public IActionResult DadosAssistencias(DateTime dInicio, DateTime dFim, int c, string cl, string vl)
+        {
+            // Retrieve data from your data source
+            FT_ManagementContext context = HttpContext.RequestServices.GetService(typeof(FT_ManagementContext)) as FT_ManagementContext;
+            PHCContext phccontext = HttpContext.RequestServices.GetService(typeof(PHCContext)) as PHCContext;
+            
+            var data = phccontext.ObterFolhasObraEstatisticas(phccontext.ObterCliente(c,0), cl, vl, dInicio, dFim);
+
+            return Ok(data);
+        }
     }
 }
