fe2ce456a4cb72c80f0f514872c672f3937667f8
Jorge Monteiro <jmonteiro@food-tech.pt>
2024-07-05 11:23:55 +0100

    correcao email

--- FT_Management/Models/NotificacoesContext.cs
+++ FT_Management/Models/NotificacoesContext.cs
@@ -558,7 +558,7 @@ namespace FT_Management.Models
 
         public static bool EnviarEmailMarcacaoPD(FolhaObra fo, Marcacao m, int Estado)
         {
-            string Assunto = "RE: " + fo.ReferenciaServico;
+            string Assunto = "RE: " + fo.ReferenciaServico + " - ";
             string Email = fo.ReferenciaServico.StartsWith("58") ? "2370@kyntech.pt" : "itsupport@jm-hello.com";
 
             //Estado 1 - Concluido | 2 - Encaminhar | 3 - Pendente
